fc14d2e6e98f3a0924b89c460bd5e1ebca58a49a
Bruna Padilha da Costa <114956460+brunapcosta@users.noreply.github.com>
2023-09-28 17:01:44 -0300

    config

--- server/download.js
+++ server/download.js
@@ -10,7 +10,7 @@ export const download = (videoId) =>
       .on("info", (info) => {
         const seconds = info.formats[0].approxDurationMs / 1000
 
-        if (seconds > 60) {
+        if (seconds > 90) {
           throw new Error("A duração desse vídeo é maior que 60 segundos")
         }
       })
